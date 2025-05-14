2168ce4a9ddc30973f44f832997967f5c1bf13ba
Margo1293 <margarita.dotsenko93@gmail.com>
2025-05-14 16:15:09 +0300

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 const body = document.querySelector('body');
+
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
