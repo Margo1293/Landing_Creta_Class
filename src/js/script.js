eedfd7c5c95ef6d4b79b31acc9faf4203678f305
Margo1293 <margarita.dotsenko93@gmail.com>
2025-05-19 15:07:22 +0300

    tab

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,27 @@ const swiper = new Swiper('.swiper', {
       },
 
 });
+
+function tabs(tabsBlockSelector) {
+	const tabsNavAll = document.querySelectorAll(`${tabsBlockSelector} [data-tab]`);
+	const tabsContentAll = document.querySelectorAll(`${tabsBlockSelector} [data-tab-content]`);
+
+	tabsNavAll.forEach(function (item) {
+		item.addEventListener('click', function (event) {
+			if (item.classList.contains('active')) return;
+
+			tabsNavAll.forEach((i) => {
+				i.classList.remove('active');
+			});
+			item.classList.add('active');
+
+			tabsContentAll.forEach((k) => {
+				k.classList.remove('active');
+			});
+			const dataTab = event.target.dataset.tab;
+			document.querySelector(`[data-tab-content]#${dataTab}`).classList.add('active');
+		});
+	});
+}
+
+tabs('.tabs__block');
